96d8e01f03aea9b5d6bf119eaf2f622d8d76fc3d
Varanasi Sai Srinivasa Karthik <vsskarthik99@gmail.com>
2025-12-17 15:00:24 +0530

    Create __init__.py

---
+++ src/hardware/__init__.py
@@ -0,0 +1,12 @@
+"""
+Hardware testing and monitoring utilities.
+
+Supports resource monitoring on various platforms including
+Raspberry Pi and other ARM-based IoT devices.
+"""
+
+__all__ = [
+    "HardwareMonitor",
+    "IoTDeviceTester",
+    "BatteryLifeEstimator",
+]
